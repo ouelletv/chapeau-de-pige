881bbdad6640e980ef42462b5ffb6aa25778fe2b
Francis G <francis@ixmedia.com>
2019-05-24 15:04:03 -0400

    Bring back ga, metas

--- dist/precache-manifest.6827c9a0576ca45eb55d81cc6d9e83db.js
+++ dist/precache-manifest.6827c9a0576ca45eb55d81cc6d9e83db.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "e5f23566d22723e64f7651da3600907b",
-    "url": "/fonts/QuicksandBookOblique.e5f23566.ttf"
+    "revision": "01b0771368354a8165f4e4f3cb488ef5",
+    "url": "/fonts/Lavanderia.01b07713.woff"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   },
   {
-    "revision": "01b0771368354a8165f4e4f3cb488ef5",
-    "url": "/fonts/Lavanderia.01b07713.woff"
+    "revision": "e5f23566d22723e64f7651da3600907b",
+    "url": "/fonts/QuicksandBookOblique.e5f23566.ttf"
   },
   {
-    "revision": "f589e81ad1c3f47f0d56",
-    "url": "/js/chunk-vendors.26d766a4.js"
+    "revision": "31206ed120f58a59a9d3",
+    "url": "/js/chunk-vendors.0012abdb.js"
   },
   {
-    "revision": "9273803d78b69fb23f211dc39885c873",
-    "url": "/img/pattern.9273803d.png"
+    "revision": "100127046931f560fe9f1a106c769de2",
+    "url": "/fonts/Lavanderia.10012704.ttf"
   },
   {
     "revision": "5f20b4566ab20c097a6eccbb5a49eb34",
@@ -28,16 +28,24 @@ self.__precacheManifest = [
     "url": "/fonts/QuicksandBookOblique.e77bf3e6.woff"
   },
   {
-    "revision": "0fd1e6fbab96215401f3809d28ad3e85",
+    "revision": "630e79dc481022c5dbd9e2c442ca265b",
     "url": "/index.html"
   },
   {
-    "revision": "ee3e396b0320c05cc61c",
-    "url": "/js/app.a2a8229a.js"
+    "revision": "9e23949f9806ac0fbe33",
+    "url": "/js/app.db166a9e.js"
   },
   {
     "revision": "4c92ba3ce8b529804a8f64d0b4414383",
     "url": "/img/le-chapeau.4c92ba3c.png"
+  },
+  {
+    "revision": "fc95ed95995a0b88d86d854efb799d4a",
+    "url": "/img/poster.fc95ed95.jpg"
+  },
+  {
+    "revision": "9273803d78b69fb23f211dc39885c873",
+    "url": "/img/pattern.9273803d.png"
   },
   {
     "revision": "930b8bb94f50205dc399c5b91ab8b5c2",
@@ -56,11 +64,7 @@ self.__precacheManifest = [
     "url": "/fonts/Lavanderia.4802d5d2.woff2"
   },
   {
-    "revision": "100127046931f560fe9f1a106c769de2",
-    "url": "/fonts/Lavanderia.10012704.ttf"
-  },
-  {
-    "revision": "ee3e396b0320c05cc61c",
-    "url": "/css/app.5d80c46d.css"
+    "revision": "9e23949f9806ac0fbe33",
+    "url": "/css/app.b70ce3b3.css"
   }
 ];
