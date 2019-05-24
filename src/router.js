0402b96af461a4acba2803b8acdc16a3e00ac8fa
Francis G <francis@ixmedia.com>
2019-05-24 14:26:58 -0400

    Bring back meta description

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Meta from 'vue-meta'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
+Vue.use(Meta)
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     // {
     //   path: '/about',
