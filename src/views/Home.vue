8b5bfbb24bf8dfe85e86f8f9ce38367f18fe4310
Vincent Ouellet <vincent@ixmedia.com>
2019-05-24 14:41:25 -0400

    Let's pige button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,22 +16,30 @@
       <span v-for="choice in choices" :key="choice" v-html="choice"></span>
     </div>
 
-    <div class="row push-t-xs-60">
-      <div class="col-xs-12">
-        <form @submit.prevent="addChoice">
+    <form @submit.prevent="addChoice">
+      <div class="row push-t-xs-60">
+        <div class="col-xs-12">
           <div class="form__fieldset">
             <div class="form__control form__control--text-button">
               <label for="text" class="form__field">
                 <input ref="input" v-model="choice" id="text" class="form__element" name="text" type="text" placeholder="Who's in!?">
-                <button class="form__element" type="submit">
+                <button class="form__element" type="button">
                   <svg class="form__element__icon svg svg--arrow-enter"><use xlink:href="#arrow-enter"></use></svg>
                 </button>
               </label>
             </div>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
+
+      <div class="row push-t-xs-40">
+        <div class="col-xs-12">
+          <div class="form__fieldset form__control--button">
+            <button class="form__element" type="button">Let's pige!</button>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
